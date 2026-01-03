f31aef22f8bca5042da16d969975c52b69004397
Param Chotaliya <162356253+Codage-of-Param@users.noreply.github.com>
2026-01-04 00:52:15 +0530

    EASY (List Question)
    Implement binary search to find the insert position for a target in a sorted array.

---
+++ Day-18/Search-Insert-Position.py
@@ -0,0 +1,29 @@
+# Question from : https://leetcode.com/problems/search-insert-position/description/
+
+def searchInsert(nums, target) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while(left<=right):
+            mid = (left + right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid -1
+            else:
+                left = mid + 1
+        
+        return left
+
+nums = [1,3,5,6]
+target = 5
+# target = 2
+# target = 7
+
+idx = searchInsert(nums,target)
+print(idx)
+
+########################################################################################
+# Time complexity is O(logn)
+# Space complexity is O(1)
+########################################################################################
